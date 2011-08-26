3bf621e9ed0f3811415f6d94cec32a6baf0e829f
keyvan <keyvan@trinix.(none)>
2011-08-26 08:40:56 -0700

    initial commit of skel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devsphere::Application.config.secret_token = 'd8e312221537399ae856594fc742a61851afb23c6eefcf29a69365220b1327c85cbf91010fd6c16328142c184b9afbca5d4b8b54a606d309bf6137ba62270fee'
