78820f6cee2e9b9b76ca5eecf8eca9d66a04dcdb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2011-08-28 15:48:12 -0700

    cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//= require jquery.easing.1.3
+//= require jquery.scrollTo
--- app/assets/javascripts/talk.js
+++ app/assets/javascripts/talk.js
@@ -2,6 +2,10 @@ $(function() {
   $('.shytoggle').click(function(){
     $(this).next().show();
     $(this).hide();
+    
+    $.scrollTo($(this).parent() , 800, {easing:'easeOutQuint'} );
+
+    
   });
   $('.shybox .hide_parent').click(function(){
     $(this).parent().hide();
