82a5d67e11871bb2817d295fd60b96b1d8d0e28a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2011-08-29 00:28:53 -0700

    building multi-upload

--- app/assets/javascripts/talk.js
+++ app/assets/javascripts/talk.js
@@ -2,13 +2,15 @@ $(function() {
   $('.shytoggle').click(function(){
     $(this).next().show();
     $(this).hide();
-    
-    //$.scrollTo($(this).parent() , 800, {easing:'easeOutQuint'} );
-
-    
   });
   $('.shybox .hide_parent').click(function(){
-    $(this).parent().hide();
-    $(this).parent().prev().show();
+    $(this).parents('.shybox').hide();
+    $(this).parents('.shybox').prev().show();
+    return false;
+  });
+
+  $('.shytoggle').click(function(){
+    $(this).next().show();
+    $(this).hide();
   });
 });
